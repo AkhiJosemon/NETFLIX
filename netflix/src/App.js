0da2e348cd662d415e049a61ad9a40569765c3f1
AkhiJosemon <akhijosemon@gmail.com>
2024-04-03 18:51:32 +0530

    first commit

---
+++ netflix/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import{ action,orginals} from './urls'
+import './App.css';
+import  Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Banner/>
+      <RowPost url={orginals} title='Netflix Orginals'/>
+      <RowPost url={action} title='Action' isSmall/>
+
+    </div>
+  )
+}
+
+export default App;
---
+++ netflix/src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY="6a0c816d63c45ebb531193095512bbe8";
+export const imageUrl='https://image.tmdb.org/t/p/original';
